c22722402bfe8fc9da12284531076ab360e2797f
suoh <66309436+SuohYume@users.noreply.github.com>
2025-09-24 16:47:07 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -339,6 +339,13 @@ export default {
         },
         customBackend: {
           "suoh专属后端": "https://subapi.miolove.dpdns.org",
+          "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
+          "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
+          "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
+          "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
+          nameless13提供: "https://www.nameless13.com",
+          subconverter作者提供: "https://sub.xeton.dev",
+          "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
           { value: "https://subapi.cmliussss.net" },
@@ -1304,5 +1311,6 @@ export default {
   }
 };
 </script>
+
 
 
